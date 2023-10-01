15a0e34bd94996d864a5c37ff891ad71e654d673
yblebon <135013954+yblebon@users.noreply.github.com>
2023-10-01 19:56:40 +0400

    Update process-files.py

--- utils/process-files.py
+++ utils/process-files.py
@@ -1,9 +1,12 @@
 import os
+import tempfile
 from PIL import Image
 
 print("Processing image files")
 print(f"Current directory: {os.getcwd()}")
 images_dir = os.path.join(os.getcwd(), "fafo", "images")
+
+tmp_dir = tempfile.mkdtemp()
 
 for fname in os.listdir(images_dir):
       print(f"processing file: {fname}")
@@ -11,5 +14,12 @@ for fname in os.listdir(images_dir):
          fpath = os.path.join(images_dir, fname)
          img = Image.open(fpath)
          print(f"File size: {img.size}")
+         orig_x, orig_y = img.size
+         factor = 800 / orig_x
+         y = factor * orig_y
+         img.resize((800, y), Image.ANTIALIAS)
+         output_img = os.path.join(tmp_dir, fname)
+         img.save(output_img, 'JPEG', quality=90)
+         print(f"New file size: {img.size}")
 
       
