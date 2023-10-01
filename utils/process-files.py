a274182f305456a73004698f5634ef601ecfffd3
yblebon <135013954+yblebon@users.noreply.github.com>
2023-10-01 16:39:30 +0400

    Update process-files.py

--- utils/process-files.py
+++ utils/process-files.py
@@ -2,5 +2,9 @@ import os
 
 print("Processing image files")
 print(f"Current directory: {os.getcwd()}")
+images_dir = os.path.join(os.getcwd(), "fafo", "images")
+
+for fname in os.listdir(images_dir):
+      print(f"processing file: {fname}")
 
       
