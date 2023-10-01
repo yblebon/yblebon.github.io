d4ae4c598029928b840f2d5c767637ef244c2818
yblebon <135013954+yblebon@users.noreply.github.com>
2023-10-01 16:51:13 +0400

    Update process-files.py

--- utils/process-files.py
+++ utils/process-files.py
@@ -1,4 +1,5 @@
-import os 
+import os
+from PIL import Image
 
 print("Processing image files")
 print(f"Current directory: {os.getcwd()}")
@@ -6,5 +7,9 @@ images_dir = os.path.join(os.getcwd(), "fafo", "images")
 
 for fname in os.listdir(images_dir):
       print(f"processing file: {fname}")
+      if fname.split(".")[1].lower() in ["jpeg", "jpg"]:
+         fpath = os.path.join(images_dir, fname)
+         img = Image.open(fpath)
+         print(f"File size: {img.size}")
 
       
